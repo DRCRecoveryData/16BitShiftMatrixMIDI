6627ac723e342645bb34aaf9c8304e67439c3d07
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-05-01 00:09:22 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,60 +1,79 @@
 #include <MIDIUSB.h>
 
-// Define pins for shift register connections
-#define DATA_PIN 2
-#define CLOCK_PIN 3
-#define LATCH_PIN 4
+// Define pin connections for 74HC165 shift registers
+#define DATA_IN_PIN   2 // Connect to pin 14 (DS) of the first 74HC165
+#define CLOCK_PIN     3 // Connect to pin 11 (SH_CP) of all 74HC165s
+#define LATCH_PIN     4 // Connect to pin 12 (ST_CP) of all 74HC165s
+#define NUM_REGISTERS 1 // Number of 74HC165 shift registers used (4 bits per register)
+
+// Define MIDI note numbers for the 4x4 button matrix
+const byte noteMap[16] = {
+  36, 37, 38, 39,
+  40, 41, 42, 43,
+  44, 45, 46, 47,
+  48, 49, 50, 51
+};
 
 void setup() {
-  // Set shift register pins as OUTPUT
-  pinMode(DATA_PIN, INPUT);
+  // Initialize shift register pins
+  pinMode(DATA_IN_PIN, INPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
 }
 
 void loop() {
   // Read button states
-  byte buttonStates[2] = {0};
-  readButtons(buttonStates);
+  uint16_t buttonStates = readButtonStates();
 
-  // Process button states
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 8; j++) {
-      int buttonIndex = i * 8 + j;
-      bool pressed = bitRead(buttonStates[i], j);
-      if (pressed) {
-        // Button pressed, send MIDI note
-        midiEventPacket_t noteOn = {0x09, 0x90 | 0, buttonIndex, 127};
-        MidiUSB.sendMIDI(noteOn);
-      } else {
-        // Button released, send MIDI note off
-        midiEventPacket_t noteOff = {0x08, 0x80 | 0, buttonIndex, 0};
-        MidiUSB.sendMIDI(noteOff);
-      }
+  // Send MIDI messages for pressed buttons
+  for (int i = 0; i < 16; i++) {
+    if (buttonStates & (1 << i)) {
+      sendNoteOn(noteMap[i], 127); // Send note on message
+    } else {
+      sendNoteOff(noteMap[i], 0); // Send note off message
     }
   }
 }
 
-void readButtons(byte* buttonStates) {
-  // Shift in button states from shift registers
+uint16_t readButtonStates() {
+  uint16_t buttonStates = 0;
+
+  // Load parallel data into the shift registers
   digitalWrite(LATCH_PIN, LOW);
   delayMicroseconds(1);
   digitalWrite(LATCH_PIN, HIGH);
-  delayMicroseconds(1);
   
-  for (int i = 0; i < 2; i++) {
-    buttonStates[i] = shiftIn(DATA_PIN, CLOCK_PIN);
+  // Read the serial data from the shift registers
+  for (int i = NUM_REGISTERS - 1; i >= 0; i--) {
+    buttonStates |= ((uint16_t)shiftIn(DATA_IN_PIN, CLOCK_PIN) << (i * 8)); // Combine bits into 16-bit integer
   }
+
+  return buttonStates;
 }
 
+// Function to read serial data from 74HC165
 byte shiftIn(int dataPin, int clockPin) {
-  byte bits = 0;
+  byte value = 0;
   for (int i = 0; i < 8; ++i) {
-    digitalWrite(clockPin, LOW);
-    delayMicroseconds(1);
-    bitWrite(bits, i, digitalRead(dataPin));
     digitalWrite(clockPin, HIGH);
     delayMicroseconds(1);
+    value |= (digitalRead(dataPin) << (7 - i));
+    digitalWrite(clockPin, LOW);
+    delayMicroseconds(1);
   }
-  return bits;
+  return value;
+}
+
+// Function to send a MIDI note on message
+void sendNoteOn(byte note, byte velocity) {
+  Serial.write(0x90); // Note on channel 1 command
+  Serial.write(note); // Note number
+  Serial.write(velocity); // Velocity
+}
+
+// Function to send a MIDI note off message
+void sendNoteOff(byte note, byte velocity) {
+  Serial.write(0x80); // Note off channel 1 command
+  Serial.write(note); // Note number
+  Serial.write(velocity); // Velocity
 }
